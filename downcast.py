887631916fbb572eec6f26794ca3c2bc161b8829
Nathan Naze <nanaze@gmail.com>
2013-11-29 23:39:03 -0600

    Submit implementation of first downcast

---
+++ downcast.py
@@ -0,0 +1,36 @@
+"""Downcast will fetch an RSS feed and write out the paths to the media
+files (MP3, etc) to standard out.
+
+Example:
+downcast.py http://feeds.thisamericanlife.org/talpodcast
+
+This can be used in conjunction with xargs/wget to download podcasts.
+
+Example:
+downcast.py <url> | xargs wget 
+"""
+
+import sys
+import urllib2
+import xml.sax
+
+
+
+def main():
+  if len(sys.argv):
+    sys.exit(__doc__)
+
+  url = sys.argv[1]
+  req = urllib2.urlopen(url)
+  handler = RssHandler()
+  result = xml.sax.parse(req, handler)
+
+class RssHandler(xml.sax.handler.ContentHandler):
+
+  def startElement(self, name, attrs):
+    if name == 'enclosure':
+      if attrs.has_key('url'):
+        print attrs.getValue('url')
+
+if __name__ == '__main__':
+  main()
